a5647e13534dc00f73a1fe0db80bdf6e3fa0bc65
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-08-30 23:22:26 +0900

    배열 시작

---
+++ array-object/js/app.js
@@ -0,0 +1,18 @@
+// var myArray = new Array("one", "two", "three", "four", "five");
+
+// for(var i=0; i<myArray.length; i++){
+//     console.log(myArray[i]);
+// }
+
+// var triple = new Array("3", "6", "9");
+// for(var z=0; z<triple.length; z++){
+//     console.log(triple[z])
+// }
+
+//어레이 메소드
+
+var nums = ["1", "2", "3"];
+var chars = ["a", "b", "c", "d"];
+var double = ["2", "4", "6", "8"];
+console.log(double.push("10"))
+console.log(double)
