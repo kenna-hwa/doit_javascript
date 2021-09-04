23814b8442f690188b16b932da7668ff1772e8eb
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-09-04 21:44:09 +0900

    quiz2 완성

--- bom/js/quiz.js
+++ bom/js/quiz.js
@@ -1,3 +1,20 @@
 
 // document.open("current.html", "width=300, height=50", "_blank")
 
+//랜덤 배경색
+function changeBg(){
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.body.style.background = bgColor;
+
+}
+
+changeBg();
+
+var btn = document.querySelector("button");
+btn.addEventListener("click", function(){
+    location.reload()
+})
